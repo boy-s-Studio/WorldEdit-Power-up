4192b83c8210505dc97ac6be7b77c9c6ea0b9667
boyboy0710 <boyboy0710@naver.com>
2021-03-01 19:13:19 +0900

    files class수정

--- src/main/java/WorldEdit_Power_up/Files.java
+++ src/main/java/WorldEdit_Power_up/Files.java
@@ -1,5 +1,15 @@
 package WorldEdit_Power_up;
 
+
 public class Files {
+
+    public static Main plugin;
+
+
+    public static void setPlugin(Main MainPlugin) {
+        plugin = MainPlugin;
+
+    }
+
 
 }
