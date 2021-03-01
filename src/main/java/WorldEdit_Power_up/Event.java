23ea1210ee4261cdd35b1377f654bbe3e1704a1d
son123j <boy0710boy@naver.com>
2021-03-01 17:17:10 +0900

    class

---
+++ src/main/java/WorldEdit_Power_up/Event.java
@@ -0,0 +1,14 @@
+package WorldEdit_Power_up;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class Event implements Listener {
+
+    @EventHandler
+    public void join(PlayerJoinEvent e) {
+
+    }
+
+}
