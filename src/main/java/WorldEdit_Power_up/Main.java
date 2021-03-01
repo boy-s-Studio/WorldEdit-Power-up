4da91699df956c89ccc81de5b0537f70979fdf86
son123j <boy0710boy@naver.com>
2021-03-01 16:01:08 +0900

    readme.md

--- src/main/java/WorldEdit_Power_up/Main.java
+++ src/main/java/WorldEdit_Power_up/Main.java
@@ -84,7 +84,6 @@ public final class Main extends JavaPlugin implements TabExecutor, Listener {
         }
         return false;
     }
-/*
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         if (command.getName().equals("")) {
@@ -101,6 +100,7 @@ public final class Main extends JavaPlugin implements TabExecutor, Listener {
         return null;
     }
 
+/*
 Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "명령어"); // 콘솔에서 명령어 실행
 Bukkit.dispatchCommand(플레이어, "명령어"); // 플레이어의 권한으로 명령어 실행
  */
