219680d868ab8535768ebfa52c9f25fe9fd04918
boyboy0710 <boyboy0710@naver.com>
2021-03-01 20:26:16 +0900

    q

--- src/main/java/WorldEdit_Power_up/Commands/Commands_test.java
+++ src/main/java/WorldEdit_Power_up/Commands/Commands_test.java
@@ -1,17 +1,10 @@
 package WorldEdit_Power_up.Commands;
 
-import com.sk89q.worldedit.IncompleteRegionException;
-import com.sk89q.worldedit.bukkit.BukkitAdapter;
-import com.sk89q.worldedit.bukkit.BukkitPlayer;
 import com.sk89q.worldedit.bukkit.WorldEditPlugin;
-import com.sk89q.worldedit.math.BlockVector3;
-import com.sk89q.worldedit.regions.Region;
-import com.sk89q.worldedit.world.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
-import org.bukkit.entity.Player;
 
 import java.util.List;
 
@@ -21,8 +14,6 @@ public class Commands_test implements CommandExecutor, TabExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
-        Player p = (Player) sender;
 
         if (label.equalsIgnoreCase("test")) {
             System.out.println("test");
