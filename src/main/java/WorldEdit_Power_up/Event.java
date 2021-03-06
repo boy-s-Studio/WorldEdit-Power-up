ef6b436a325e6758066aba4e9357d70789fa7383
son123j <boy0710boy@naver.com>
2021-03-06 16:28:13 +0900

    block break

--- src/main/java/WorldEdit_Power_up/Event.java
+++ src/main/java/WorldEdit_Power_up/Event.java
@@ -2,6 +2,7 @@ package WorldEdit_Power_up;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class Event implements Listener {
@@ -9,6 +10,13 @@ public class Event implements Listener {
     @EventHandler
     public void join(PlayerJoinEvent e) {
         e.setJoinMessage(e.getPlayer() + "님이 입장하셨습니다");
+    }
+    
+    @EventHandler
+    public void BlockBreakEvent(BlockBreakEvent e) {
+    
+    
+    
     }
 
 }
