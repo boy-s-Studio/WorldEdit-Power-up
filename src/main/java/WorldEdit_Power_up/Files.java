18abd781aeda7f5764a08e3f69a4506b8ebb75e9
boyboy0710 <boyboy0710@naver.com>
2021-03-12 16:17:01 +0900

    data.yml 추가

--- src/main/java/WorldEdit_Power_up/Files.java
+++ src/main/java/WorldEdit_Power_up/Files.java
@@ -1,72 +1,60 @@
 package WorldEdit_Power_up;
 
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-
-import java.io.*;
-import java.util.HashMap;
-import java.util.UUID;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
 
 public class Files {
 
-    public static Main plugin;
+    private Main plugin;
+    private FileConfiguration dataconfig = null;
+    private File configFile = null;
 
-    private final File f = new File(plugin.getDataFolder(), "config.yml");
-
-    public static void setPlugin(Main MainPlugin) {
-        plugin = MainPlugin;
-
+    public void DataManager(Main plugin) {
+        this.plugin = plugin;
+        plugin.saveDefaultConfig();
     }
 
-    public void makeFile(File f) {
-        if (!f.exists() || !f.isFile()) {
-            try {
-                f.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+    public void reloadConfig() {
+        if(this.configFile == null) {
+            this.configFile = new File(this.plugin.getDataFolder(), "data.yml");
+        }
+
+        this.dataconfig = YamlConfiguration.loadConfiguration(this.configFile);
+
+        InputStream defaultStream = this.plugin.getResource("data.yml");
+        if(defaultStream != null) {
+            YamlConfiguration defaultconfig = YamlConfiguration.loadConfiguration(new InputStreamReader(defaultStream));
+            this.dataconfig.setDefaults(defaultconfig);
         }
     }
 
-    @SuppressWarnings("deprecation")
-    public void mapToFile(File f, HashMap<UUID, Integer> map) {
-        Bukkit.getScheduler().scheduleAsyncRepeatingTask(plugin, new Runnable() {
-
-            @Override
-            public void run() {
-                try {
-                    plugin.console.sendMessage(ChatColor.GREEN + "데이터를 저장 했습니다");
-                    FileWriter writer = new FileWriter(f, false); // Map to File
-                    for (UUID uuid : map.keySet()) {
-                        writer.write(uuid.toString() + "|" + map.get(uuid) + "\n");
-                    }
-                    writer.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, 20 * 30, 20 * 30);
-
+    public FileConfiguration getConfig() {
+        if(this.dataconfig == null) reloadConfig();
+        return this.dataconfig;
     }
 
-    public void fileToMap(File f, HashMap<UUID, Integer> map) {
+    public void saveConfig() {
+        if(this.dataconfig == null || this.configFile == null) return;
+
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(f)); // File to Map
-            String fileLine = null;
-            while ((fileLine = reader.readLine()) != null) {
-
-                UUID uuid = UUID.fromString(fileLine.split("\\|")[0]);
-                String str = fileLine.split("\\|")[1];
-
-                map.put(uuid, Integer.parseInt(str));
-            }
-        } catch (FileNotFoundException e3) {
-            e3.printStackTrace();
-        } catch (IOException e4) {
-            e4.printStackTrace();
+            this.getConfig().save(this.configFile);
+        } catch (IOException e) {
+            this.plugin.getLogger().log(Level.SEVERE, "파일을 저장할수 없습니다", e);
         }
     }
 
+    public void saveDefaultConfig() {
+        if(this.configFile == null) this.configFile = new File(this.plugin.getDataFolder(), "data.yml");
+
+        if(!this.configFile.exists()) {
+            this.plugin.saveResource("data.yml", false);
+        }
+    }
 
 }
