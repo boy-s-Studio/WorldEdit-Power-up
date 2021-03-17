bb2613cc803e1c555fe6277716ce046c9ece370e
son123j <boy0710boy@naver.com>
2021-03-17 14:49:38 +0900

    config_test 주석

--- src/main/java/WorldEdit_Power_up/config_File/config_test.java
+++ src/main/java/WorldEdit_Power_up/config_File/config_test.java
@@ -16,16 +16,13 @@ public class config_test implements CommandExecutor, TabExecutor {
 
         if(label.equalsIgnoreCase("config_test")) {
                 String s = "와 대성공";
-                //config를 불러온다
-                String test = Main.getPlugin(Main.class).getConfig().getString("test");
-                //config 값을 설정한다
-                Main.getPlugin(Main.class).getConfig().set("test", s);
-                //컨피그 값을 저장한다
-                Main.getPlugin(Main.class).saveConfig();
-                //컨피그를 리로드 한다
-                Main.getPlugin(Main.class).reloadConfig();
-                //프린트 한다
-                System.out.println(test);
+                
+                String test = Main.getPlugin(Main.class).getConfig().getString("test");  //config를 불러온다
+                Main.getPlugin(Main.class).getConfig().set("test", s);  //config 값을 설정한디
+                Main.getPlugin(Main.class).saveConfig();  //컨피그 값을 저장한다
+                Main.getPlugin(Main.class).reloadConfig();  //컨피그를 리로드 한다
+            
+                System.out.println(test);  //프린트 한다
 
             }
 
