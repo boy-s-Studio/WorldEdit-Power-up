7e7f2c5cb891926606726e13c5b04acad0d73be9
son123j <boy0710boy@naver.com>
2021-03-18 14:10:54 +0900

    +

--- src/main/java/WorldEdit_Power_up/others/Easter_Egg.java
+++ src/main/java/WorldEdit_Power_up/others/Easter_Egg.java
@@ -42,27 +42,6 @@ public class Easter_Egg implements CommandExecutor, TabExecutor {
             }
             
         }
-    
-        if(cmd.getName().equalsIgnoreCase("+")) {
-    
-            int num_1 = 0;
-            int num_2 = 0;
-            int num = 0;
-    
-            num = num_1 + num_2;
-            
-            if(args[0].length() != 0) {
-                num_1 = Integer.parseInt(args[2]);
-            }
-    
-            if(args[1].length() != 1) {
-                num_2 = Integer.parseInt(args[2]);
-            }
-            
-            sender.sendMessage(num + "");
-            System.out.println(num);
-            
-        }
         
         return false;
     }
