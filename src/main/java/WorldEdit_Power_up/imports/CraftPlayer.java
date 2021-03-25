e2e9b56617a9e3c8bc7c741521912d91658a44df
boyboy0710 <boyboy0710@naver.com>
2021-03-25 16:50:05 +0900

    imports 추가 및 CraftPlayer 테스트

--- src/main/java/WorldEdit_Power_up/imports/CraftPlayer.java
+++ src/main/java/WorldEdit_Power_up/imports/CraftPlayer.java
@@ -1,4 +1,10 @@
 package WorldEdit_Power_up.imports;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+
 public class CraftPlayer {
+
+    public void CraftPlayer(Player p, CraftPlayer cp) {
+    }
 }
